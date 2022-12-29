a7f1a4d744003cb08d017aa3ac05b21aa25f5df8
Ali Demirci <ademirci@phinion.com>
2022-12-29 11:20:11 +0100

    If conditions

--- Day3/ifConditions.py
+++ Day3/ifConditions.py
@@ -1,7 +1,31 @@
 # create a function that checks if the customer can get access to buy ticket or not. It should be at least 18 years old to get access.
 
-age= int(input("Write your age \n"))
-if age>=18:
+age = int(input("Write your age \n"))
+if age >= 18:
     print("You can buy the ticket")
 else:
     print("You are younger than 18. You can not buy a ticket from us.")
+
+# Say if number odd or even
+
+newNumber = int(input("Write a number"))
+
+if newNumber % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+
+# Check height if >120cm  can write else cant write if can write check age>18 price $12 else price $7
+
+customerHeight = int(input("Write your height \n"))
+
+adultPrice = 12
+childPrice = 7
+if customerHeight >= 120:
+    customerAge = int(input("Write your age \n"))
+    if customerAge >= 18:
+        print(f"Ticket price is {adultPrice}")
+    else:
+        print(f"Ticket price is {childPrice}")
+else:
+    print("You are not tall enough to ride.")
--- calculateBMI.py
+++ calculateBMI.py
@@ -3,7 +3,18 @@ weight = input("Add your weight as kg")
 
 def calculate(height,weight):
         BMI=int(weight)/float(height)**2
-        return round(BMI,2) # 2 makes decimal number
+        convertedBMI=round(BMI,2) # 2 makes decimal number
+        if convertedBMI<18.5:
+            return f"{convertedBMI} You are underweight"
+        elif convertedBMI<25:
+            return f"{convertedBMI} You have a normal weight"
+        elif convertedBMI<30:
+            return f"{convertedBMI} You are overweight"
+        elif convertedBMI<35:
+            return f"{convertedBMI} You are obese"
+        else:
+            return f"{convertedBMI} You are clinically obese"
+        
         #return '{0:.2f}'.format(BMI)  we can use this also for decimals
 
 print(calculate(height,weight))
