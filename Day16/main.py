433ca9e24ea3efb4d37048eca776645ebdf57abe
Ali Demirci <ademirci@phinion.com>
2023-01-11 17:53:55 +0100

    Complete day 16 with OOP Coffee machine

---
+++ Day16/main.py
@@ -0,0 +1,39 @@
+# from turtle import Turtle, Screen
+# ciko= Turtle()
+# ciko.shape("turtle")
+# ciko.color("red")
+# ciko.forward(100)
+# print(ciko)
+#
+# my_screen= Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
+# from prettytable import PrettyTable
+# table = PrettyTable()
+# table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
+# table.add_column("Type", ["Electric", "Water", "Fire"])
+# print(table)
+from menu import Menu
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+menu = Menu()
+
+is_on = True
+
+while is_on:
+    options = menu.get_items()
+    choice = input(f"What would you like? ({options}): ")
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(choice)
+
+        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+            coffee_maker.make_coffee(drink)
+
