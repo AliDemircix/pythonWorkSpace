e87adaf26ce5deb3224f91d6b8a137304f1e17a4
Ali Demirci <ademirci@phinion.com>
2023-01-27 15:14:12 +0100

    Create right paddle

--- Day22/main.py
+++ Day22/main.py
@@ -1,21 +1,37 @@
-from turtle import Screen
+from turtle import Screen,Turtle
 
 import time
 
 screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
-screen.title("My snake game")
+screen.title("My pong game")
 screen.tracer(0)
 
+paddle=Turtle()
+paddle.shape("square")
+paddle.color("white")
+paddle.shapesize(stretch_wid=5,stretch_len=1)
+paddle.penup()
+paddle.goto(350,0)
 
+
+def go_up():
+    new_y=paddle.ycor()+20
+    paddle.goto(paddle.xcor(),new_y)
+
+def go_down():
+    new_y=paddle.ycor()-20
+    paddle.goto(paddle.xcor(),new_y)
 
 screen.listen()
-
+screen.onkey(go_down,"Down")
+screen.onkey(go_up,"Up")
 
 
 game_is_on = True
 
-
+while game_is_on:
+    screen.update()
 
 screen.exitonclick()
