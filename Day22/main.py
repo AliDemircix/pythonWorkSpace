4450336718992f43ec595814b0b1a09534f55523
Ali Demirci <ademirci@phinion.com>
2023-01-27 15:31:55 +0100

    Create paddle class to create left paddle

--- Day22/main.py
+++ Day22/main.py
@@ -1,6 +1,5 @@
-from turtle import Screen,Turtle
-
-import time
+from turtle import Screen, Turtle
+from paddle import Paddle
 
 screen = Screen()
 screen.setup(width=800, height=600)
@@ -8,26 +7,14 @@ screen.bgcolor("black")
 screen.title("My pong game")
 screen.tracer(0)
 
-paddle=Turtle()
-paddle.shape("square")
-paddle.color("white")
-paddle.shapesize(stretch_wid=5,stretch_len=1)
-paddle.penup()
-paddle.goto(350,0)
-
-
-def go_up():
-    new_y=paddle.ycor()+20
-    paddle.goto(paddle.xcor(),new_y)
-
-def go_down():
-    new_y=paddle.ycor()-20
-    paddle.goto(paddle.xcor(),new_y)
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
 
 screen.listen()
-screen.onkey(go_down,"Down")
-screen.onkey(go_up,"Up")
-
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(l_paddle.go_down, "s")
+screen.onkey(l_paddle.go_up, "w")
 
 game_is_on = True
 
