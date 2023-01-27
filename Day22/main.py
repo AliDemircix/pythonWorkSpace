e1752221f3b186cf855a51a115d3d5ce880c8069
Ali Demirci <ademirci@phinion.com>
2023-01-27 16:03:52 +0100

    Change ball direction after click to any paddle

--- Day22/main.py
+++ Day22/main.py
@@ -25,4 +25,13 @@ while game_is_on:
     screen.update()
     ball.move()
 
+    # detect collision with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+    # detect collision with r_paddle
+    if ball.distance(r_paddle)<50 and ball.xcor()>320 or ball.distance(l_paddle)<50 and ball.xcor()>-320:
+        ball.bounce_x()
+    
+
+
 screen.exitonclick()
