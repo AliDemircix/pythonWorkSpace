401e64588d9f60eaa061789ebff5b7909d78d04e
Ali Demirci <ademirci@phinion.com>
2023-01-27 14:58:07 +0100

    Finish snake game

--- Day21/main.py
+++ Day21/main.py
@@ -30,11 +30,19 @@ while game_is_on:
     # Detect collision with food
     if snake.head.distance(food) < 15:
         food.refresh()
+        snake.extend()
         scoreboard.increase_score()
     # Detect collision with wall
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
         game_is_on = False
         scoreboard.game_over()
+    # Deteck tail
+    # give as list not including first segment
+    for segment in snake.segments[1:]:
+
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
 
 
 screen.exitonclick()
