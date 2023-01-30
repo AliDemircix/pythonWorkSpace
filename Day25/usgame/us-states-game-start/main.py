6c40b87e5e526aa95b23142d01d0cbb7fcd03863
Ali Demirci <ademirci@phinion.com>
2023-01-30 13:50:15 +0100

    List and dict comprehensions

--- Day25/usgame/us-states-game-start/main.py
+++ Day25/usgame/us-states-game-start/main.py
@@ -21,10 +21,11 @@ while len(guessed_states)<50:
     answer_state=screen.textinput(title=f"{len(guessed_states)}/50 State was Correct",prompt="What's another states name?").title() # title makes all word first letter capitalize
 
     if answer_state=="Exit":
-        missing_states=[]
-        for state in all_states:
-            if state not in guessed_states:
-                missing_states.append(state)
+        # missing_states=[]
+        # for state in all_states:
+        #     if state not in guessed_states:
+        #         missing_states.append(state)
+        missing_states=[state for state in all_states if state not in guessed_states]
         new_data = pd.DataFrame(missing_states)
         new_data.to_csv("Day25/usgame/us-states-game-start/states_to_learn.csv")
         break
