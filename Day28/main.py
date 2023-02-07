6c80e81f092a922d1519e6d387dc8718fcdc6a51
Ali Demirci <ademirci@phinion.com>
2023-02-07 21:32:40 +0100

    Complete pomodoro app

--- Day28/main.py
+++ Day28/main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import math
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -8,24 +9,85 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
+timer = None
 
 # ---------------------------- TIMER RESET ------------------------------- #
 
+
+def reset_timer():
+    window.after_cancel(timer)
+    canvas.itemconfig(timer_text,text="00:00")
+    timer_label.config(text="Timer")
+    check_icon.config(text="")
+    global reps
+    reps=0
+
+
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 
+
+def start_timer():
+    global reps
+    global start_text
+    reps += 1
+    work_sec = WORK_MIN*60
+    short_break_sec = SHORT_BREAK_MIN*60
+    long_break_sec = LONG_BREAK_MIN*60
+
+    if reps % 8 == 0:
+        count_down(long_break_sec)
+        timer_label.config(text="Long Break", fg=RED)
+    elif reps % 2 == 0:
+        count_down(short_break_sec)
+        timer_label.config(text="Break", fg=PINK)
+    else:
+        count_down(work_sec)
+        timer_label.config(text="Work", fg=GREEN)
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+
+
+def count_down(count):
+    global reps
+    count_min = math.floor(count/60)
+    if count_min < 10:
+        count_min = f"0{count_min}"
+    count_sec = count % 60
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
+
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+    if count > 0:
+        global timer
+        timer = window.after(1000, count_down, count-1)
+    else:
+        start_timer()
+        mark = ""
+        for _ in range(math.floor(reps/2)):
+            mark += "✔"
+        check_icon.config(text=mark)
+
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
 
-canvas = Canvas(width=202, height=224, bg=YELLOW,highlightthickness=0)
+canvas = Canvas(width=202, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="Day28/tomato.png")
 canvas.create_image(102, 110, image=tomato_img)
-canvas.create_text(110, 140, text="00:00", fill="white",
+timer_text = canvas.create_text(110, 140, text="00:00", fill="white",
+                                font=(FONT_NAME, 35, "bold"))
+canvas.grid(column=1, row=1)
+timer_label = Label(text="Timer", fg=GREEN, bg=YELLOW,
+                    font=(FONT_NAME, 35, "bold"))
+timer_label.grid(column=1, row=0)
+start_btn = Button(text="Start", highlightthickness=0, command=start_timer)
+start_btn.grid(column=0, row=2)
+reset_btn = Button(text="Reset", highlightthickness=0, command=reset_timer)
+reset_btn.grid(column=2, row=2)
+check_icon = Label(fg=GREEN, bg=YELLOW,
                    font=(FONT_NAME, 35, "bold"))
-canvas.pack()
-
-
+check_icon.grid(column=1, row=3)
 window.mainloop()
