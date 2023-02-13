7d3e905af9f462607cb0e23b8e79b0361b8be8ff
Ali Demirci <ademirci@phinion.com>
2023-02-13 17:14:41 +0100

    Fetch data from api

--- Day33/response.py
+++ Day33/response.py
@@ -11,3 +11,11 @@ response=requests.get(url="http://api.open-notify.org/iss-now.json")
 response.raise_for_status()
 data=response.json()
 print(data)
+parameters={
+    "lat":52.1613,
+    "lng":4.505
+}
+getsun_info=requests.get("https://api.sunrise-sunset.org/json",params=parameters)
+sunset=getsun_info.json()["results"]["sunset"]
+sunrise=getsun_info.json()["results"]["sunrise"]
+print(sunset,sunrise)
